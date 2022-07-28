a153142924061939b8574ce2fd304d019c0e9a1d
Habibe Sayrac <hsayracode@gmail.com>
2022-07-28 19:13:08 +0300

    Initial commit

---
+++ src/Match.java
@@ -0,0 +1,17 @@
+public class Match {
+    String homeTeam;
+    String awayTeam;
+
+    public Match(String homeTeam, String awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+}
